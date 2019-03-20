115f3729214c2f2610f8b1f7b7f086533c3c6c1b
Christof Seiler <christof.seiler@gmail.com>
2019-03-20 12:34:36 +0100

    made analysis of results independent of cytof data

--- install.R
+++ install.R
@@ -3,6 +3,6 @@ devtools::install_github("RGLab/ggcyto", ref="trunk")
 pkgs_needed = c("devtools","tidyverse","magrittr","SummarizedExperiment",
                 "ggthemes","cowplot","RColorBrewer","broom","hexbin",
                 "intergraph","igraph","ggnetwork","ggcorrplot","MASS",
-                "parallel","dplyr","knitr")
+                "parallel","dplyr","knitr","dagitty","ggdag")
 source("http://bioconductor.org/biocLite.R")
 biocLite(pkgs_needed)
